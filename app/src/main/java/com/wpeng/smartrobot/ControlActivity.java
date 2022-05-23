db2653341807929ad21f8aec5a07cd4215b29b67
WPENG <WPENGxs@163.com>
2022-05-23 08:37:08 +0800

    20220523 update

--- app/src/main/java/com/wpeng/smartrobot/ControlActivity.java
+++ app/src/main/java/com/wpeng/smartrobot/ControlActivity.java
@@ -1,13 +1,21 @@
 package com.wpeng.smartrobot;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
@@ -23,8 +31,11 @@ public class ControlActivity extends AppCompatActivity {
     JSONObject jsonObject;
     String data;
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
-    @SuppressLint("HandlerLeak")
+
+    @SuppressLint({"HandlerLeak", "UseSwitchCompatOrMaterialCode"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +47,7 @@ public class ControlActivity extends AppCompatActivity {
             public void handleMessage(Message message){
                 switch (message.what){
                     case 0x01:
-                        hum.setText("湿度为:"+data);
+                        hum.setText(data);
                         break;
                     case 0x02:
                         Toast.makeText(ControlActivity.this,"获取失败，请检查服务器连接!",Toast.LENGTH_SHORT).show();
@@ -53,6 +64,91 @@ public class ControlActivity extends AppCompatActivity {
             }
         };
         handler.postDelayed(runnable,0);//一个0秒的定时器（因为获取数据需要时间，索性就不写延时了）
+
+        sharedPreferences=getSharedPreferences("hum_d",MODE_PRIVATE);
+        editor=sharedPreferences.edit();
+
+        Switch hum_switch=findViewById(R.id.hum_switch);
+        hum_switch.setChecked(sharedPreferences.getBoolean("hum_d",false));
+        hum_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putBoolean("hum_d", !hum_switch.isSelected());
+                editor.apply();
+            }
+        });
+
+        Button hum_btu=findViewById(R.id.hum_btu);
+        hum_btu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(ControlActivity.this);
+                dialog.setTitle("设置范围");
+
+                LinearLayout linearLayout=new LinearLayout(ControlActivity.this);
+                EditText up=new EditText(ControlActivity.this);
+                up.setHint("上限");
+                EditText down=new EditText(ControlActivity.this);
+                down.setHint("下限");
+                linearLayout.addView(up);
+                linearLayout.addView(down);
+
+                dialog.setView(linearLayout);
+                dialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String up_str=up.getText().toString();
+                        String down_str=down.getText().toString();
+                        hum_btu.setText(up_str+" - "+down_str);
+                        Toast.makeText(ControlActivity.this,"设置完毕",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        sharedPreferences=getSharedPreferences("light_d",MODE_PRIVATE);
+
+        Switch light_switch=findViewById(R.id.light_switch);
+        light_switch.setChecked(sharedPreferences.getBoolean("light_d",false));
+        light_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putBoolean("light_d", !hum_switch.isSelected());
+                editor.apply();
+            }
+        });
+
+        Button light_btu=findViewById(R.id.light_btu);
+        light_btu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(ControlActivity.this);
+                dialog.setTitle("设置范围");
+
+                LinearLayout linearLayout=new LinearLayout(ControlActivity.this);
+                EditText up=new EditText(ControlActivity.this);
+                up.setHint("上限");
+                EditText down=new EditText(ControlActivity.this);
+                down.setHint("下限");
+                linearLayout.addView(up);
+                linearLayout.addView(down);
+
+                dialog.setView(linearLayout);
+                dialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String up_str=up.getText().toString();
+                        String down_str=down.getText().toString();
+                        light_btu.setText(up_str+" - "+down_str);
+                        Toast.makeText(ControlActivity.this,"设置完毕",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.show();
+            }
+        });
 
     }
 
